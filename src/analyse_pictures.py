0f750f42f4974632899f776afbdd63b0bf2973c3
Eric Breitenstein <eric.breitenstein@gmx.ch>
2022-12-27 17:31:07 +0100

    added analyse pictures

---
+++ src/analyse_pictures.py
@@ -0,0 +1,43 @@
+import os
+from plot_helper.plot_helper import PlotPieces
+from calibration.calibration import calibrate, calibrateImages, croppImages
+from data_management.DataManager import DataManager
+import pickle
+from calc.ErrorCalculator import calc_error_and_rot_mat
+import matplotlib.pyplot as plt
+
+
+CALIB_PICTURES_PATH = os.path.join("C:",os.path.sep,"Users","TV","Pictures","Puzzel_3000_Pieces","Calibrated_Pictures_1")
+
+def main():
+    path = CALIB_PICTURES_PATH
+    piecesListPath = os.path.join(path,"pieces.pickle")
+    errorDictPath = os.path.join(path,"error_dict.pickle")
+    m = DataManager()
+    plotter = PlotPieces()
+    if os.path.exists(piecesListPath):
+        pieces = m.loadPieces(piecesListPath)
+    else:
+        pieces = m.generatePieces(path)
+        m.storePieces(piecesListPath, pieces)
+
+    plotter.plot_all(pieces)
+    plt.show()
+    if os.path.exists(errorDictPath):
+        with open(errorDictPath,"rb") as f:
+            obj = pickle.load(f)
+        error = obj["error"]
+        transformMat = obj["transformMat"]
+    else:
+        error,transformMat = calc_error_and_rot_mat(pieces)
+        obj = {"error":error,"transformMat":transformMat}
+        with open(errorDictPath,"wb") as f:
+            pickle.dump(obj,f)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
